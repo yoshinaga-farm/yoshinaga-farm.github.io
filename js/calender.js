25432bbc5b6b6f83652c770b0656aa7b642c6f1a
Ken-Yoshinaga1 <k8963813@gmail.com>
2026-03-12 08:46:27 +0900

    fix

--- js/calender.js
+++ js/calender.js
@@ -1,3 +1,4 @@
 const now = new Date();
 const month = now.getMonth() + 1;
-document.getElementById("month-title").textContent = month + "月の野菜";
+document.getElementById("main__title--monthly").textContent = month + "月の野菜";
+console.log(month);
