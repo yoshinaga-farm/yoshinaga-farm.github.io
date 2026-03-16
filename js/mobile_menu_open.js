3bda601c9f16769c7305cf32d880f9199150198e
Ken-Yoshinaga1 <k8963813@gmail.com>
2026-03-16 13:47:42 +0900

    fix

---
+++ js/mobile_menu_open.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector('.header__nav');
+const menu = document.querySelector('.menu');
+
+toggle.addEventListener('click', () => {
+    menu.classList.toggle('open');  // openクラスのON/OFF
+});
