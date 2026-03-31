e65102ec1937c1f650cba2db9f406fd6cdc0e0a3
Ken-Yoshinaga1 <k8963813@gmail.com>
2026-03-31 11:51:21 +0900

    時間

--- js/live-cam-time.js
+++ js/live-cam-time.js
@@ -1,9 +1,14 @@
 const liveCamHour = () => {
   const now = new Date();
   const hour = now.getHours();
-  const minute =now.getMinutes();
+  const minute = now.getMinutes();
+  const second = now.getSeconds();
+  const msec = now.getMilliseconds();
+  
   document.getElementById("live-cam-hour").textContent =
-    `販売所のライブ映像(${hour}:${minute})`;
+    `販売所のライブ映像(${hour}:${minute}:${second}.${msec})`;
 };
 
 liveCamHour();
+
+setInterval(liveCamHour, 1);
