f63894c2f37c3fe6a136781aecd84eac345cb051
Ken-Yoshinaga1 <k8963813@gmail.com>
2026-04-02 12:05:22 +0900

    曜日訂正

--- js/live-cam-time.js
+++ js/live-cam-time.js
@@ -11,14 +11,13 @@ const liveCamHour = () => {
   const week = now.getDay();
 
   const weekList = [
+    "日曜日",
     "月曜日",
     "火曜日",
     "水曜日",
     "木曜日",
     "金曜日",
     "土曜日",
-    "日曜日",
-    
   ];
   const weekStr = weekList[week];
 
@@ -28,4 +27,4 @@ const liveCamHour = () => {
 
 liveCamHour();
 
-setInterval(liveCamHour,10);
+setInterval(liveCamHour, 10);
